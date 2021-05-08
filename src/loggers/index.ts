8489a63b18f4c49b8a8664def02220125e5aa7ec
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-09 02:22:47 +0800

    chore(logger): turn back to 'info'

--- src/loggers/index.ts
+++ src/loggers/index.ts
@@ -2,6 +2,6 @@ import pino from "pino";
 
 export const rootLogger = pino({
   name: "schweb-crawler",
-  level: "debug",
+  level: "info",
 });
 export const logger = (component: string) => rootLogger.child({component});
