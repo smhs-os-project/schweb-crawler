ce11ed20231fd40b4bb8b3469f9115cf07e32975
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-08 17:56:18 +0800

    fix(utils/getParseCheerio): to compatible with Node.js

--- src/utils/getParseCheerio.ts
+++ src/utils/getParseCheerio.ts
@@ -1,4 +1,5 @@
 import cheerio, { CheerioAPI } from "cheerio";
+import fetch from "node-fetch";
 
 export async function getParseCheerio(url: string): Promise<CheerioAPI> {
   const html = await fetch(url).then(r => r.text());
