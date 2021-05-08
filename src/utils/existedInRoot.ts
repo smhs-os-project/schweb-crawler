db348ab689cfc052a63d31eed466b00dfaddc6fa
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-09 02:45:36 +0800

    refactor(utils/existedInRoot): add logging

--- src/utils/existedInRoot.ts
+++ src/utils/existedInRoot.ts
@@ -1,7 +1,13 @@
 import { existsSync } from "fs";
 import { join } from "path";
 import { DATA_ROOT } from "../consts";
+import { logger } from "../loggers";
 
 export function existedInRoot(relativePath: string) {
-  return existsSync(join(DATA_ROOT, relativePath));
+  const log = logger("utils.existedInRoot");
+  const pathToDetermine = join(DATA_ROOT, relativePath);
+  const isExist = existsSync(pathToDetermine);
+
+  log.debug(`${pathToDetermine} is existed? ${isExist}`);
+  return isExist;
 }
