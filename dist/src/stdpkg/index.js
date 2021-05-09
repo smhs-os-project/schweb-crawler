e81792bc3f3892d7f4d99025c3168edc816bdf5a
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-09 13:49:34 +0800

    bump(): 1.1.1

--- dist/src/stdpkg/index.js
+++ dist/src/stdpkg/index.js
@@ -7,7 +7,6 @@ exports.createStandardPackageFile = exports.serializeStandardPackageData = void 
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const loggers_1 = require("../loggers");
-const existedInRoot_1 = require("../utils/existedInRoot");
 function serializeStandardPackageData({ data }) {
     const log = loggers_1.logger("stdpkg.index.serializeStandardPackageData");
     log.debug("serializing a StandardPackage");
@@ -21,15 +20,10 @@ async function createStandardPackageFile(root, pkg) {
     const log = loggers_1.logger("stdpkg.index.createStandardPackageFile");
     const filePath = path_1.default.join(root, pkg.filename);
     log.debug("creating folders that wasn't created before");
-    const parentFolder = path_1.default.join(pkg.filename, "..");
-    log.debug("  - checking if the folder has been created");
-    if (!existedInRoot_1.existedInRoot(parentFolder)) {
-        log.debug("    - NO, create it then continue");
+    const parentFolder = path_1.default.join(filePath, "..");
+    if (!fs_1.default.existsSync(parentFolder))
         await fs_1.default.promises.mkdir(parentFolder, { recursive: true });
-    }
-    else
-        log.debug("    - YES, continue");
-    log.info(`writing the serialized standard package data to ${filePath}`);
+    log.debug(`writing the serialized standard package data to ${filePath}`);
     return fs_1.default.promises.writeFile(filePath, serializeStandardPackageData(pkg));
 }
 exports.createStandardPackageFile = createStandardPackageFile;
