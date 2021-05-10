08f87f95f2de3efb03f065e0d1eb456dec077e72
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-10 22:39:33 +0800

    feat(loggers): add extra field

--- src/loggers/index.ts
+++ src/loggers/index.ts
@@ -2,6 +2,12 @@ import pino from "pino";
 
 export const rootLogger = pino({
   name: "schweb-crawler",
-  level: "info",
+  level: "debug",
 });
-export const logger = (component: string) => rootLogger.child({component});
+
+/**
+ * Create a logger for specified components or stages.
+ * @param component The component namespace.
+ * @param extra The extra information.
+ */
+export const logger = (component: string, extra: Record<string, unknown> = {}) => rootLogger.child({component, ...extra});
