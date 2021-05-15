5417c842bd451d41635f51db46303e8b5c5ce84c
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-15 18:14:20 +0800

    bump(): 1.2.2

--- dist/src/processors/Announcements/index.js
+++ dist/src/processors/Announcements/index.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AnnouncementsProcessors = exports.ContestAnnouncements = exports.StudyAnnouncements = exports.GradeAnnouncements = exports.SchoolAnnouncements = exports.LawAnnouncements = void 0;
 const Announcements_1 = __importDefault(require("../libs/Announcements"));
-exports.LawAnnouncements = Announcements_1.default("#Dyn_1_2 a", "law");
-exports.SchoolAnnouncements = Announcements_1.default("#Dyn_2_1 a", "school");
-exports.GradeAnnouncements = Announcements_1.default("#Dyn_2_2 a", "grade");
-exports.StudyAnnouncements = Announcements_1.default("#Dyn_2_3 a", "study");
-exports.ContestAnnouncements = Announcements_1.default("#Dyn_1_2_1 a", "contest");
+exports.LawAnnouncements = Announcements_1.default("#Dyn_1_2 .mtitle > a", "law");
+exports.SchoolAnnouncements = Announcements_1.default("#Dyn_2_1 .mtitle > a", "school");
+exports.GradeAnnouncements = Announcements_1.default("#Dyn_2_2 .mtitle > a", "grade");
+exports.StudyAnnouncements = Announcements_1.default("#Dyn_2_3 .mtitle > a", "study");
+exports.ContestAnnouncements = Announcements_1.default("#Dyn_1_2_1 .mtitle > a", "contest");
 exports.AnnouncementsProcessors = [
     exports.LawAnnouncements,
     exports.SchoolAnnouncements,
