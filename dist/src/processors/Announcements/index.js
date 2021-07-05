fe56346740d6b9e22513f3d4d12ef47288b59109
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-05 12:43:05 +0800

    build(dist): 1.2.5

--- dist/src/processors/Announcements/index.js
+++ dist/src/processors/Announcements/index.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AnnouncementsProcessors = exports.ContestAnnouncements = exports.DocumentsAnnouncements = exports.StudyAnnouncements = exports.GradeAnnouncements = exports.WirelessAndTimetableAnnouncements = exports.SchoolAnnouncements = exports.LawAnnouncements = void 0;
+exports.AnnouncementsProcessors = exports.DocumentsAnnouncements = exports.StudyAnnouncements = exports.ContestAnnouncements = exports.LawAnnouncements = exports.GradeAnnouncements = exports.WirelessAndTimetableAnnouncements = exports.SchoolAnnouncements = void 0;
 const Announcements_1 = __importDefault(require("../libs/Announcements"));
-exports.LawAnnouncements = Announcements_1.default("#Dyn_1_2 .mtitle > a", "law");
 exports.SchoolAnnouncements = Announcements_1.default("#Dyn_2_1 .mtitle > a", "school");
 exports.WirelessAndTimetableAnnouncements = Announcements_1.default("#Dyn_2_2 .mtitle > a", "wireless-and-timetable");
 exports.GradeAnnouncements = Announcements_1.default("#Dyn_2_3 .mtitle > a", "grade");
-exports.StudyAnnouncements = Announcements_1.default("#Dyn_2_4 .mtitle > a", "study");
-exports.DocumentsAnnouncements = Announcements_1.default("#Dyn_1_1_1 .mtitle > a", "documents");
+exports.LawAnnouncements = Announcements_1.default("#Dyn_1_1_1 .mtitle > a", "law");
 exports.ContestAnnouncements = Announcements_1.default("#Dyn_1_2_1 .mtitle > a", "contest");
+exports.StudyAnnouncements = Announcements_1.default("#Dyn_1_2_2 .mtitle > a", "study");
+exports.DocumentsAnnouncements = Announcements_1.default("#Dyn_1_1_2 .mtitle > a", "documents");
 exports.AnnouncementsProcessors = [
     exports.LawAnnouncements,
     exports.SchoolAnnouncements,
