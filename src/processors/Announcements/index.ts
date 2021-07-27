779e7686cd506e6258243beeda1ccc3892fe8f78
pan93412 <28441561+pan93412@users.noreply.github.com>
2021-07-27 21:33:26 +0800

    feat(processors/Announcements/index): update block selector

--- src/processors/Announcements/index.ts
+++ src/processors/Announcements/index.ts
@@ -4,12 +4,16 @@ export const SchoolAnnouncements = AnnouncementWrapper(
   "#Dyn_2_1 .mtitle > a",
   "school"
 );
-export const WirelessAndTimetableAnnouncements = AnnouncementWrapper(
+export const WirelessAnnouncements = AnnouncementWrapper(
   "#Dyn_2_2 .mtitle > a",
-  "wireless-and-timetable"
+  "wireless"
+);
+export const TimetableAnnouncements = AnnouncementWrapper(
+  "#Dyn_2_3 .mtitle > a",
+  "timetable"
 );
 export const GradeAnnouncements = AnnouncementWrapper(
-  "#Dyn_2_3 .mtitle > a",
+  "#Dyn_2_4 .mtitle > a",
   "grade"
 );
 export const LawAnnouncements = AnnouncementWrapper(
@@ -20,19 +24,20 @@ export const ContestAnnouncements = AnnouncementWrapper(
   "#Dyn_1_2_1 .mtitle > a",
   "contest"
 );
-export const StudyAnnouncements = AnnouncementWrapper(
-  "#Dyn_1_2_2 .mtitle > a",
-  "study"
-);
 export const DocumentsAnnouncements = AnnouncementWrapper(
   "#Dyn_1_1_2 .mtitle > a",
   "documents"
+);
+export const StudyAnnouncements = AnnouncementWrapper(
+  "#Dyn_1_2_2 .mtitle > a",
+  "study"
 );
 
 export const AnnouncementsProcessors = [
   LawAnnouncements,
   SchoolAnnouncements,
-  WirelessAndTimetableAnnouncements,
+  WirelessAnnouncements,
+  TimetableAnnouncements,
   GradeAnnouncements,
   StudyAnnouncements,
   DocumentsAnnouncements,
