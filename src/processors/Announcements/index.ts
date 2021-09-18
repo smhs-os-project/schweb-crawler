54d1c34a68f2ff1d1a027994a79a24788e4591f9
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-19 03:29:49 +0800

    refactor(processors/Announcements): use the real block name
    we no longer need to change the class manually; it can automatically fetch the announcement

--- src/processors/Announcements/index.ts
+++ src/processors/Announcements/index.ts
@@ -1,43 +1,29 @@
 import AnnouncementWrapper from "../libs/Announcements";
 
 export const SchoolAnnouncements = AnnouncementWrapper(
-  "#Dyn_2_1 .mtitle > a",
+  "校內訊息公告區",
   "school"
 );
-export const TimetableAnnouncements = AnnouncementWrapper(
-  "#Dyn_2_2 .mtitle > a",
-  "timetable"
+export const NetworkAnnouncements = AnnouncementWrapper(
+  "網路公告區",
+  "network"
 );
-export const WirelessAnnouncements = AnnouncementWrapper(
-  "#Dyn_2_3 .mtitle > a",
-  "wireless"
-);
-export const GradeAnnouncements = AnnouncementWrapper(
-  "#Dyn_2_4 .mtitle > a",
-  "grade"
-);
-export const LawAnnouncements = AnnouncementWrapper(
-  "#Dyn_1_1_1 .mtitle > a",
-  "law"
-);
+export const GradeAnnouncements = AnnouncementWrapper("升學公告區", "grade");
+export const LawAnnouncements = AnnouncementWrapper("學校法規公告區", "law");
 export const ContestAnnouncements = AnnouncementWrapper(
-  "#Dyn_1_2_1 .mtitle > a",
+  "競賽活動公告區",
   "contest"
 );
 export const DocumentsAnnouncements = AnnouncementWrapper(
-  "#Dyn_1_1_2 .mtitle > a",
+  "公文公告區",
   "documents"
 );
-export const StudyAnnouncements = AnnouncementWrapper(
-  "#Dyn_1_2_2 .mtitle > a",
-  "study"
-);
+export const StudyAnnouncements = AnnouncementWrapper("研習公告區", "study");
 
 export const AnnouncementsProcessors = [
   LawAnnouncements,
   SchoolAnnouncements,
-  WirelessAnnouncements,
-  TimetableAnnouncements,
+  NetworkAnnouncements,
   GradeAnnouncements,
   StudyAnnouncements,
   DocumentsAnnouncements,
