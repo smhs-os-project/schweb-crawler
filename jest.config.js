ea9d90d96c4c882aa47cbd974f39470cee3062c7
pan93412 <pan93412@gmail.com>
2021-10-18 20:20:42 +0800

    fix(jest): the ESM support

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  // https://kulshekhar.github.io/ts-jest/docs/guides/esm-support
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
