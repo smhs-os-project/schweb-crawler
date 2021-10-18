29c0f59e721b6c613de8950f22d174484a3526d6
pan93412 <pan93412@gmail.com>
2021-10-18 20:22:11 +0800

    bump: 1.4.1

--- dist/jest.config.d.ts
+++ dist/jest.config.d.ts
@@ -1,3 +1,15 @@
-export const preset: string;
-export const testEnvironment: string;
+declare var _default: {
+    preset: string;
+    testEnvironment: string;
+    testPathIgnorePatterns: string[];
+    globals: {
+        'ts-jest': {
+            useESM: boolean;
+        };
+    };
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': string;
+    };
+};
+export default _default;
 //# sourceMappingURL=jest.config.d.ts.map
--- dist/jest.config.js
+++ dist/jest.config.js
@@ -1,6 +1,14 @@
-"use strict";
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
 };
 //# sourceMappingURL=jest.config.js.map
